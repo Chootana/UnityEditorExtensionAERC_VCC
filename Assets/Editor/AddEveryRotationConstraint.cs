31d35386cf8f6286b81ea728a33981f245860da8
chootana <t.anthony2074@gmail.com>
2025-06-21 21:30:48 +0900

    (feat) first commit

---
+++ Assets/Editor/AddEveryRotationConstraint.cs
@@ -0,0 +1,255 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Linq;
+using UnityEngine.Animations;
+
+public class AddEveryRotationConstraint : EditorWindow
+{
+    [SerializeField]
+    GameObject fromJoint = null;
+
+    [SerializeField]
+    GameObject toJoint = null;
+
+    private ConstraintSource fromConstraintSource;
+    private bool includeInActive = false;
+
+    private string parentName = "None";
+    private int childrenSize = 0; 
+
+    private enum CONSTRAINT_TYPE
+    {
+        RotationConstraint,
+        ParentConstraint,
+        PositionConstraint,
+    }
+
+    private CONSTRAINT_TYPE constraintType = CONSTRAINT_TYPE.RotationConstraint;
+
+
+    [MenuItem("Window/Extension Tools/Add Every Rotation Constraint")]
+    static void Open()
+    {
+        GetWindow<AddEveryRotationConstraint>();
+    }
+
+    private void OnGUI()
+    {
+
+
+        GUILayout.Label("", EditorStyles.boldLabel);
+
+
+
+        Color defaultColor = GUI.backgroundColor;
+        using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+        {
+            GUI.backgroundColor = Color.gray;
+            using (new GUILayout.HorizontalScope(EditorStyles.toolbar))
+            {
+                GUILayout.Label("Original Avatar", EditorStyles.whiteLabel);
+            }
+            GUI.backgroundColor = defaultColor;
+
+            EditorGUI.indentLevel++;
+            fromJoint = EditorGUILayout.ObjectField("GameObject (Joint)", fromJoint, typeof(GameObject), true) as GameObject;
+            ShowJointProperty(fromJoint);
+
+            EditorGUI.indentLevel--;
+
+        }
+
+
+        GUILayout.Label("", EditorStyles.boldLabel);
+
+
+        using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+        {
+            GUI.backgroundColor = Color.gray;
+            using (new GUILayout.HorizontalScope(EditorStyles.toolbar))
+            {
+                GUILayout.Label("Target Avatar", EditorStyles.whiteLabel);
+            }
+            GUI.backgroundColor = defaultColor;
+
+            EditorGUI.indentLevel++;
+            toJoint = EditorGUILayout.ObjectField("GameObject (Joint)", toJoint, typeof(GameObject), true) as GameObject;
+            ShowJointProperty(toJoint);
+            EditorGUI.indentLevel--;
+        }
+        GUI.backgroundColor = defaultColor;
+
+
+        GUILayout.Label("", EditorStyles.boldLabel);
+
+        constraintType = (CONSTRAINT_TYPE)EditorGUILayout.EnumPopup("Constraint Type", constraintType);
+
+
+        using (new GUILayout.HorizontalScope(GUI.skin.box))
+        {
+            GUI.backgroundColor = Color.green;
+            if (GUILayout.Button("Copy"))
+            {
+                Copy(fromJoint, toJoint);   
+            }
+            GUI.backgroundColor = defaultColor;
+
+
+            GUI.backgroundColor = Color.white;
+            if (GUILayout.Button("Reset"))  
+            {
+                Reset(toJoint);
+            }
+            GUI.backgroundColor = defaultColor;
+        }
+    }
+
+    void Show(GameObject gameObject)
+    {
+        Debug.Log(gameObject.name);
+    }
+
+    void ShowJointProperty(GameObject obj)
+    {
+        parentName = GetParentName(obj);
+        childrenSize = GetChildrenSize(obj);
+        EditorGUILayout.LabelField("Root Name", parentName);
+        EditorGUILayout.LabelField("Number of Joints", childrenSize.ToString());
+
+    }
+
+    string GetParentName(GameObject obj)
+    {
+        if (obj == null)
+        {
+            return "None";
+        }
+        
+        string nameRoot = obj.transform.root.gameObject.name.ToString();
+        return nameRoot;
+        
+    }
+
+
+    int GetChildrenSize(GameObject obj)
+    {
+        if (obj == null)
+        {
+            return 0;
+        }
+
+        int childrenSize = obj.GetComponentsInChildren<Transform>(includeInActive).Length;
+        return childrenSize;
+    }
+
+    void Copy(GameObject fromJoint, GameObject toJoint)
+    {
+        if (fromJoint == null)
+        {
+            Debug.LogError("GameObject(from) not found");
+        }
+
+        if (toJoint == null)
+        {
+            Debug.LogError("GameObject(to) not found");
+        }
+
+        Transform[] fromChildren = fromJoint.GetComponentsInChildren<Transform>(includeInActive);
+        Transform[] toChildren = toJoint.GetComponentsInChildren<Transform>(includeInActive);
+
+
+        if (fromChildren.Length != toChildren.Length)
+        {
+            Debug.LogError("Mismatch of the number of joints, (from): " + fromChildren.Length + " != (to): " + toChildren.Length);
+            return;
+        }
+
+        ResetConstraintSource(fromConstraintSource);
+        fromConstraintSource.weight = 1.0f;
+
+
+        foreach (var (joint, index) in toChildren.Select((x, i) => (x, i)))
+        {
+
+            switch(constraintType)
+            {
+                case CONSTRAINT_TYPE.RotationConstraint:
+                    RotationConstraint rotationConstraint = joint.gameObject.GetComponent<RotationConstraint>();
+                    if (rotationConstraint == null) rotationConstraint = joint.gameObject.AddComponent<RotationConstraint>();
+                    if (rotationConstraint.sourceCount > 0) continue;
+                    fromConstraintSource.sourceTransform = fromChildren[index];
+                    rotationConstraint.AddSource(fromConstraintSource);
+                    rotationConstraint.constraintActive = true;
+                    break;
+
+                case CONSTRAINT_TYPE.ParentConstraint:
+                    ParentConstraint parentConstraint = joint.gameObject.GetComponent<ParentConstraint>();
+                    if (parentConstraint == null) parentConstraint = joint.gameObject.AddComponent<ParentConstraint>();
+                    if (parentConstraint.sourceCount > 0) continue;
+                    fromConstraintSource.sourceTransform = fromChildren[index];
+                    parentConstraint.AddSource(fromConstraintSource);
+                    parentConstraint.constraintActive = true;
+                    break;
+
+                case CONSTRAINT_TYPE.PositionConstraint:
+                    PositionConstraint positionConstraint = joint.gameObject.GetComponent<PositionConstraint>();
+                    if (positionConstraint == null) positionConstraint = joint.gameObject.AddComponent<PositionConstraint>();
+                    if (positionConstraint.sourceCount > 0) continue;
+                    fromConstraintSource.sourceTransform = fromChildren[index];
+                    positionConstraint.AddSource(fromConstraintSource);
+                    positionConstraint.constraintActive = true;
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+
+        Debug.Log($"Add {constraintType} to Every Joints");
+        ResetConstraintSource(fromConstraintSource);
+        return;
+    }
+
+
+
+    void ResetConstraintSource(ConstraintSource constraintSource)
+    {
+        constraintSource.sourceTransform = null;
+    }
+
+
+    void Reset(GameObject toJoints)
+    {
+        if (toJoints == null)
+        {
+            Debug.LogError("GameObject(from) not found");
+            return;
+        }
+
+        switch(constraintType)
+        {
+            case CONSTRAINT_TYPE.RotationConstraint:
+                RotationConstraint[] rotationConstraints = toJoints.gameObject.GetComponentsInChildren<RotationConstraint>(includeInActive);
+                foreach (RotationConstraint rotationConstraint in rotationConstraints) GameObject.DestroyImmediate(rotationConstraint);
+                break;
+
+            case CONSTRAINT_TYPE.ParentConstraint:
+                ParentConstraint[] parentConstraints = toJoints.gameObject.GetComponentsInChildren<ParentConstraint>(includeInActive);
+                foreach (ParentConstraint parentConstraint in parentConstraints) GameObject.DestroyImmediate(parentConstraint);
+                break;
+
+            case CONSTRAINT_TYPE.PositionConstraint:
+                PositionConstraint[] positionConstraints = toJoints.gameObject.GetComponentsInChildren<PositionConstraint>(includeInActive);
+                foreach (PositionConstraint positionConstraint in positionConstraints) GameObject.DestroyImmediate(positionConstraint);
+                break;
+
+            default:
+                break;
+
+        }
+
+        Debug.Log($"Remove {constraintType}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,88 @@
+# All Every Rotation Constraint (AERC)
+**階層構造のあるゲームオブジェクトにRotation Constraintを簡単に追加・設定するためのエディタ拡張**
+
+ソース（コピー元）のゲームオブジェクトとターゲット（コピー先）のゲームオブジェクトを指定すると，ターゲットの子オブジェクトそれぞれに対して，
+
+    1. 対応するゲームオブジェクトにAdd Component (Rotation Constraint)する
+    2. Is Active を On にする
+    3. Constraint Settingsに同じ階層に存在するソースオブジェクトのTransformを追加する
+
+を繰り返し行ってくれる．
+
+# How to Use
+アバターにサブアームを装着することを例とする．
+
+今回はサブアームを導入は既に済んでるとして，腕の関節それぞれにRotation Constraintを追加して同期させる．
+
+
+（サブアーム導入方法は[コチラ](https://booth.pm/ja/items/2203578)をおススメします．）
+
+
+1. まずUnityでUnityEditor_AERC.unitypackageをインポートする．
+    - Assets直下にEditorディレクトリがあることを確認する．
+
+2. インポート後，Unity上部のWindowタブにExtension Toolsが表示されるようになり，その下のAdd Every Rotation Constraintをクリックする．
+    ![Window/ExtensionTools](https://user-images.githubusercontent.com/44863813/102009739-d7c23480-3d7c-11eb-83c4-d99650156c21.png)
+
+- 項目の説明
+
+    - Original Avatar: コピー元を指定する箇所
+    - Target Abatar: コピー先を指定する箇所
+    - GameObject(Joint): Rotation Constraintを追加したいゲームオブジェクトを指定する
+    - Root Name: 指定したゲームオブジェクトの最上層の親の名前が表示される
+    - Number of Joints: 指定したゲームオブジェクトとその子オブジェクトの総計が表示される
+
+    ![Explanation/Window](https://user-images.githubusercontent.com/44863813/102009753-fc1e1100-3d7c-11eb-9ba6-ec54bcbb8715.png)
+
+
+3. **Original Avatar**の方にコピー元となる腕（例えば本体アバターのSoulder.L）を，**Target Avatar**の方にコピー先となる腕（例えばサブアームのSoulder.L）をそれぞれに指定する．
+    - この時Number of Jointsの値が一致しているか確認する．
+    - もし一致していなかったら階層的構造的に対応していないゲームオブジェクトが含まれているので，そのオブジェクトを非アクティブにする．
+    
+    ![Explain/AddGameObjectAndRun](https://user-images.githubusercontent.com/44863813/102012996-ac494500-3d90-11eb-98f1-142a2774141e.png)
+
+4. **Copy**ボタンをクリックする．
+
+5. 肩から指先まですべてに**Rotation Constraint**が追加される.
+    - Is Activeにチェックが入る
+    - Constraint Settingsに**Original Avatar**の対応するTransformが追加される
+
+    ![Explain/AfterCopy](https://user-images.githubusercontent.com/44863813/102013550-4494f900-3d94-11eb-910c-e249345239cf.png)
+
+6. 右腕（Soulder.R）も同様に行う.
+
+7. 3.の時に非アクティブにしたゲームオブジェクトがあれば，アクティブに戻す
+
+8. 完成！
+
+    - **Reset**ボタンをクリックすると，Target Avatar側のゲームオブジェクト（及び子オブジェクト）に存在するRotation Constraintが削除される．
+
+
+# Examples of Use
+VRChat用のアバター改変をする時に，以下のような作業が簡単になる！
+
+
+- サブアームの追加する
+- 頭にアバターを追加し，両腕を本体と同期させる
+- アバターのフルトラッキング適正向上（後日追記）
+- 複数アバターのアニメーションを同期させる
+
+などなど
+
+これらの作業を行うと両腕だけで40回程度のポチポチ作業が必要になるので，めんどくさい！時間短縮したいと思った方はこのエディタ拡張をお試しあれ！
+
+![Result](https://user-images.githubusercontent.com/44863813/102007590-d2112280-3d6d-11eb-9870-5ca335b6973c.png)
+
+では善きVRChatライフを！
+
+# Notes
+説明のため[ロポリこんちゃん](https://mido0021.booth.pm)，[稲荷原](https://booth.pm/ja/items/2297510)，[メカアーム](https://booth.pm/ja/items/1221319)を使用したが，本プロジェクトに含まれていない．
+
+# Author
+chootana (ちゅーたな)
+
+twitter: [@choo_zap](https://twitter.com/choo_zap)
+
+# License
+
+This software is released under the MIT License, see LICENSE.txt
